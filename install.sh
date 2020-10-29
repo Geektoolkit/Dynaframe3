c64a1efa8a9573df419d60adf88cb0944a9672be
DESKTOP-OI3A9NA\jfarro <joefarro@hotmail.com>
2020-10-28 23:20:55 -0700

    Adding UpgradeScript / capability

--- install.sh
+++ install.sh
@@ -1,20 +1,61 @@
+ #!/bin/bash
+version=$1
+file="/home/pi/Dynaframe/appsettings.json"
+
 GREEN='\033[1;32m'
 CYAN='\033[0;96m'
+RED='\033[0;31m'
 NC='\033[0m' #no color
 
-echo -e "${GREEN} Geektoolkit present: Dynaframe 2.0 ${NC}"
+echo "Version is: $version"
+if [ -z "$1" ]
+then
+  echo -e  "${CYAN}No version passed in...using default${NC}"
+  version="2.07"
+fi
+
+
+echo -e "${GREEN}=========================================================="
+echo -e "  -- Geektoolkit present: Dynaframe 2.0                 --"
+echo -e "  -- Preparing to install Version: $version                 --"
+echo -e "  -- Find out more on youtube on the ${CYAN}Geektookit ${GREEN}channel --"
+echo -e "  -- Coding: Joe Farro  Special thanks: qwksilver       --"
+echo -e "  -- Powered by Avalonia.                               --"
+echo -e "==========================================================${NC}"
+if [ -f "$file" ];
+then
+  echo -e "${GREEN} Found old appsettings..backing up ${NC}"
+  cp $file "/home/pi/"
+fi
+
 echo "Cleaning up before we begin..."
+
+cd /home/pi/
 rm -rf /home/pi/Dynaframe
 rm -rf /home/pi/.config/autostart/dynaframe.desktop
+
+echo -e "${GREEN}Installing a few tools before we begin (unclutter/unzip) ${NC}"
 sudo apt-get install unzip
 sudo apt-get install unclutter
 mkdir -p  /home/pi/Dynaframe
 cd Dynaframe
+
 sudo chmod 777 .
+
 echo -e "${GREEN}Grabbing the files from github...hold please..${NC}"
-wget https://github.com/Geektoolkit/Dynaframe3/releases/download/2.05/Dynaframe205.zip
-echo "Unzipping them!"
-unzip Dynaframe205.zip
+
+wget "https://github.com/Geektoolkit/Dynaframe3/releases/download/$version/Dynaframe2.zip"
+
+if [ -f "/home/pi/Dynaframe/Dynaframe2.zip" ]
+then
+  echo -e "${GREEN}Successfully downloaded archive from Github!${NC}"
+else
+  echo -e "${RED}FAILURE! Dynaframe2.zip did not download successfully ${NC}"
+  exit 1
+fi
+
+echo -e "${GREEN}Unzipping Dynaframe2.zip ${NC}"
+unzip Dynaframe2.zip
 echo -e "${GREEN}Adding Execution Permissions to Dynaframe ${NC}"
 sudo chmod +x Dynaframe
 sudo chmod +x run.sh
@@ -22,7 +63,12 @@ echo -e "${GREEN}Setting it up to autostart ${NC}"
 mkdir -p /home/pi/.config/autostart
 sudo cp dynaframe.desktop  /home/pi/.config/autostart
 echo -e "${GREEN}cleaning up zip file ${NC}"
-rm Dynaframe205.zip
-
-
-
+rm Dynaframe2.zip
+if [ -f "/home/pi/appsettings.json" ];
+then
+  echo -e "${GREEN}Restoring old appsettings...${NC}"
+  cd /home/pi/
+  cp "appsettings.json" "/home/pi/Dynaframe"
+fi
+cp "/home/pi/Dynaframe/install.sh" "/home/pi/UpgradeDynaframe.sh"
+chmod +x /home/pi/UpgradeDynaframe.sh
