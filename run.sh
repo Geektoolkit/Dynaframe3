48c2efd90e11ca00628ba4cabbfa9e0fd4167185
qwksilver <qwksilver@iname.com>
2020-12-13 01:54:28 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,6 +6,8 @@
 # sleep, or possibly syncing files.
 # unclutter is installed by install.sh and used to hide the mouse cursor
 # setterm is used to prevent screen blanking
+#
+# filename/date article on bin/bash/scripting https://www.cyberciti.biz/faq/unix-linux-appleosx-bsd-shell-appending-date-to-filename/
 
 unclutter -idle 2 &
 cd /home/pi/Dynaframe
